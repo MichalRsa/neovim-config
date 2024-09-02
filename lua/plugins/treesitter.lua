468b37eea6102a7246254926b5ebc9e8dbcb0427
MichalRsa <michalrosa17@gmail.com>
2024-09-02 13:52:31 +0200

    treesitter set up for blade files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,8 +3,19 @@ return {
   build = ":TSUpdate",
   config = function()
     local configs = require("nvim-treesitter.configs")
+
+    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+    parser_config.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+      filetype = "blade"
+    }
+
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "blade" },
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
