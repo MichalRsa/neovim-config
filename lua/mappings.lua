b9b2bb593e87a092968a61f6b9db6e3745f6bbe3
MichalRsa <michalrosa17@gmail.com>
2025-10-10 09:36:20 +0200

    adds which-key plugin

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,6 +1,8 @@
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", ":", ";", { desc = "Repeat last f/t/F/T" })
+
 map("i", "jk", "<ESC>")
 
 map("n", "<leader>/", "gcc", { desc = "Toggle Comment", remap = true })
