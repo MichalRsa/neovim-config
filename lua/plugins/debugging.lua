0d89536566ae8061168c925531d520b15776d35b
MichalRsa <michalrosa17@gmail.com>
2025-11-09 14:40:44 +0100

    adds dap config for php

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,32 +1,88 @@
 return {
-	-- "mfussenegger/nvim-dap",
-	-- dependencies = {
-	-- 	"rcarriga/nvim-dap-ui",
-	-- },
-	--
-	-- config = function()
-	-- 	local dap = require("dap")
-	-- 	local dapui = require("dapui")
-	--
-	-- 	dap.listeners.before.attach.dapui_config = function()
-	-- 		dapui.open()
-	-- 	end
-	-- 	dap.listeners.before.launch.dapui_config = function()
-	-- 		dapui.open()
-	-- 	end
-	-- 	dap.listeners.before.event_terminated.dapui_config = function()
-	-- 		dapui.close()
-	-- 	end
-	-- 	dap.listeners.before.event_exited.dapui_config = function()
-	-- 		dapui.close()
-	-- 	end
-	--
-	-- 	vim.keymap.set("n", "<Leader>b", function()
-	-- 		dap.toggle_breakpoint()
-	-- 	end)
-	--
-	-- 	vim.keymap.set("n", "<F5>", function()
-	-- 		dap.continue()
-	-- 	end)
-	-- end,
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		{
+			"rcarriga/nvim-dap-ui",
+			dependencies = { "nvim-neotest/nvim-nio" },
+		},
+		"nvim-telescope/telescope-dap.nvim",
+	},
+
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+
+		dapui.setup()
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		require("telescope").load_extension("dap")
+		dap.adapters.php = {
+			type = "executable",
+			command = "node",
+			args = { vim.fn.stdpath("config") .. "/vscode-php-debug/out/phpDebug.js" },
+		}
+
+		dap.configurations.php = {
+			{
+				type = "php",
+				request = "launch",
+				name = "Listen for Xdebug",
+				port = 9003,
+			},
+		}
+
+		vim.keymap.set("n", "<F5>", function()
+			require("dap").continue()
+		end)
+		vim.keymap.set("n", "<F6>", function()
+			require("dap").step_over()
+		end)
+		vim.keymap.set("n", "<F7>", function()
+			require("dap").step_into()
+		end)
+		vim.keymap.set("n", "<F8>", function()
+			require("dap").step_out()
+		end)
+		vim.keymap.set("n", "<Leader>b", function()
+			require("dap").toggle_breakpoint()
+		end)
+		vim.keymap.set("n", "<Leader>B", function()
+			require("dap").set_breakpoint()
+		end)
+		vim.keymap.set("n", "<Leader>lp", function()
+			require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+		end)
+		vim.keymap.set("n", "<Leader>dr", function()
+			require("dap").repl.open()
+		end)
+		vim.keymap.set("n", "<Leader>dl", function()
+			require("dap").run_last()
+		end)
+		vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+			require("dap.ui.widgets").hover()
+		end)
+		vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
+			require("dap.ui.widgets").preview()
+		end)
+		vim.keymap.set("n", "<Leader>df", function()
+			local widgets = require("dap.ui.widgets")
+			widgets.centered_float(widgets.frames)
+		end)
+		vim.keymap.set("n", "<Leader>ds", function()
+			local widgets = require("dap.ui.widgets")
+			widgets.centered_float(widgets.scopes)
+		end)
+	end,
 }
